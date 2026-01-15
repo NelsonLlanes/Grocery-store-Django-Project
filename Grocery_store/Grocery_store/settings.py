a376b79d3f7eabea3cc5da56babf338a72cdfb71
Nelson Llanes <nelsoncanada23@gmail.com>
2026-01-14 20:31:21 -0800

    finish first version off final exam project

--- Grocery_store/Grocery_store/settings.py
+++ Grocery_store/Grocery_store/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# logout
+LOGIN_URL = "/accounts/login/"
+#dashboard
+LOGIN_REDIRECT_URL = "/dashboard/"
+
+# login
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 # Application definition
 
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'Grocery_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
